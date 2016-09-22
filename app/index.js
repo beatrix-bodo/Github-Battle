8ba9abf2b3fe057843ad02ab5b71c45b8907d756
Beatrix Bodo <beatrix.bodo@gmail.com>
2016-09-22 11:18:23 +0200

    Add github profile picture, link and name

--- app/index.js
+++ app/index.js
@@ -1,15 +1,54 @@
+var USER_DATA = {
+  name: "Beatrix Bodó",
+  username: "beatrix-bodo",
+  image: "https://avatars0.githubusercontent.com/u/10792235?v=3&s=466"
+}
+
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
   render: function () {
     return (
-      <div>Hello World</div>
-    )
+      <div>
+        <a href={"https://www.github.com/" + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function () {
+    return <div>{this.props.name}</div>
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    );
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user="USER_DATA" />,
   document.getElementById("app"),
+);
+
+ReactDOM.render(
+  <Avatar user={USER_DATA} />,
+  document.getElementById('app')
 );
